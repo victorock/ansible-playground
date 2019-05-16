b2351086acb041223271c3c31484b833128db84c
Victor da Costa <victorockeiro@gmail.com>
2019-05-16 16:54:41 +0200

    ansible-2.7.10

--- module_utils/plugin_facts.py
+++ module_utils/plugin_facts.py
@@ -1,4 +1,8 @@
 #
+# coding: utf-8 -*-
+# (c) 2019, Victor da Costa <victorockeiro@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 import os
 from ansible import constants as C
 import ansible.plugins.loader as plugin_loader
